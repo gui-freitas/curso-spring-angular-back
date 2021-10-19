d3c8e395a8c7c01daadff939bb6cb4f3fd58b21e
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-10-19 17:54:26 -0300

    Mapeando as entidades Cliente e Servico

---
+++ src/main/java/com/gapp/clientes/model/entity/Cliente.java
@@ -0,0 +1,67 @@
+package com.gapp.clientes.model.entity;
+
+import java.time.Instant;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 80)
+	private String nome;
+	
+	@Column(nullable = false, length = 11)
+	private String cpf;
+	
+	private Instant dataCadastro;
+	
+	public Cliente() { }
+	
+	public Cliente(Integer id, String nome, String cpf, Instant dataCadastro) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataCadastro = dataCadastro;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Instant getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Instant dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+}
