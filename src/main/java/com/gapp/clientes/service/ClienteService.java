ee05eaace2291b9d09e1d9b25fe8849aa95c4754
Guilherme Freitas <guilherme.a.freitas@outlook.com>
2021-10-22 18:14:49 -0300

    Busca de usuario com metodo GET

--- src/main/java/com/gapp/clientes/service/ClienteService.java
+++ src/main/java/com/gapp/clientes/service/ClienteService.java
@@ -1,7 +1,9 @@
 package com.gapp.clientes.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.gapp.clientes.model.entity.Cliente;
 import com.gapp.clientes.model.entity.repository.ClienteRepository;
@@ -14,5 +16,11 @@ public class ClienteService {
 	
 	public Cliente salvar(Cliente cliente) {
 		return clienteRepository.save(cliente);
+	}
+
+	public Cliente buscaPorId(Integer id) {
+		return clienteRepository
+				.findById(id)
+				.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 }
